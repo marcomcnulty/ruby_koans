3274f1c2af4601098557b59cb193e3354fe90942
marcomcnulty <marcomcnultycodes@gmail.com>
2024-07-20 23:43:29 +0100

    📦 NEW: Completed about nil

--- koans/about_nil.rb
+++ koans/about_nil.rb
@@ -1,8 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+# TIL:
+# - Ruby allows classes to overide the equality operator.
+
 class AboutNil < Neo::Koan
   def test_nil_is_an_object
-    assert_equal __, nil.is_a?(Object), "Unlike NULL in other languages"
+    assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
   end
 
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
@@ -13,18 +16,18 @@ class AboutNil < Neo::Koan
       nil.some_method_nil_doesnt_know_about
     rescue Exception => ex
       # What exception has been caught?
-      assert_equal __, ex.class
+      assert_equal NoMethodError, ex.class
 
       # What message was attached to the exception?
       # (HINT: replace __ with part of the error message.)
-      assert_match(/__/, ex.message)
+      assert_match(/undefined method `some_method_nil_doesnt_know_about' for nil/, ex.message)
     end
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    assert_equal true, nil.nil?
+    assert_equal "", nil.to_s
+    assert_equal "nil", nil.inspect
 
     # THINK ABOUT IT:
     #
@@ -33,6 +36,10 @@ class AboutNil < Neo::Koan
     # or
     #    obj == nil
     # Why?
-  end
 
+    # A: obj.nil? is more readable and the intention is clearer. As as result it is also more idiomatic in Ruby code.
+    #    Further, Ruby allows classes to define their own custom logic for equality by overriding the `==` method.
+    #    This enables developers to specify what attributes or conditions determine when two instances of a class are
+    #    considered equal.
+  end
 end
