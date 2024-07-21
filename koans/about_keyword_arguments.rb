e2090ff19ff56f4f83d34f5d9ff4288962ad04c1
marcomcnulty <marcomcnultycodes@gmail.com>
2024-07-21 22:05:49 +0100

    📦 NEW: Completed about keyword arguments

--- koans/about_keyword_arguments.rb
+++ koans/about_keyword_arguments.rb
@@ -1,5 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+# TIL:
+# - Methods with named parameters can be called without any arguments and the values belonging to the parameters will be
+#   used by default.
+# - Methods can have mandatory parameters and optional parameters but the optional params must come after the named ones.
+# - By providing a named parameter without a value, you make it a mandatory named parameter.
+
 class AboutKeywordArguments < Neo::Koan
 
   def method_with_keyword_arguments(one: 1, two: 'two')
@@ -7,10 +13,10 @@ class AboutKeywordArguments < Neo::Koan
   end
 
   def test_keyword_arguments
-    assert_equal __, method_with_keyword_arguments.class
-    assert_equal __, method_with_keyword_arguments
-    assert_equal __, method_with_keyword_arguments(one: 'one')
-    assert_equal __, method_with_keyword_arguments(two: 2)
+    assert_equal Array, method_with_keyword_arguments.class
+    assert_equal [1, "two"], method_with_keyword_arguments
+    assert_equal ["one", "two"], method_with_keyword_arguments(one: 'one')
+    assert_equal [1,2], method_with_keyword_arguments(two: 2)
   end
 
   def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
@@ -18,10 +24,10 @@ class AboutKeywordArguments < Neo::Koan
   end
 
   def test_keyword_arguments_with_wrong_number_of_arguments
-    exception = assert_raise (___) do
+    exception = assert_raise (ArgumentError) do
       method_with_keyword_arguments_with_mandatory_argument
     end
-    assert_match(/__/, exception.message)
+    assert_match(/wrong number of arguments/, exception.message)
   end
 
   def method_with_mandatory_keyword_arguments(one:, two: 'two')
@@ -29,15 +35,15 @@ class AboutKeywordArguments < Neo::Koan
   end
 
   def test_mandatory_keyword_arguments
-    assert_equal __, method_with_mandatory_keyword_arguments(one: 'one')
-    assert_equal __, method_with_mandatory_keyword_arguments(two: 2, one: 1)
+    assert_equal ["one", "two"], method_with_mandatory_keyword_arguments(one: 'one')
+    assert_equal [1,2], method_with_mandatory_keyword_arguments(two: 2, one: 1)
   end
 
   def test_mandatory_keyword_arguments_without_mandatory_argument
-    exception = assert_raise(___) do
+    exception = assert_raise(ArgumentError) do
       method_with_mandatory_keyword_arguments
     end
-    assert_match(/__/, exception.message)
+    assert_match(/missing keyword: :one/, exception.message)
   end
 
 end
