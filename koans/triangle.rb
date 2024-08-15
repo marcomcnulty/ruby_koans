48964dd3d6dceca675ef5a578ce12485c6fc5f29
marcomcnulty <marcomcnultycodes@gmail.com>
2024-08-15 09:19:15 +0100

    📖 DOC: Added TIL

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -12,7 +12,17 @@
 #   about_triangle_project.rb
 # and
 #   about_triangle_project_2.rb
-#
+
+# TIL:
+# - There are six key rules a triangle must satisfy:
+#   1. Angle Sum Property - The sum of the interior angles of a triangle is always 180 degrees.
+#   2. Non-degeneracy - No side of the triangle can have a length of zero.
+#   3. Positive Area - The area of a triangle must be greater than zero, which implies that the triangle cannot be flat.
+#   4. Distinct points - A triangle's vertices must be distinct points.
+#   5. Internal and External Angle Rules - An exterior angle of a triangle is equal to the sum of the two opposite
+#      interior angles.
+#   6. Triangle inequality theorem - The sum of any two numbers is always greater than the third.
+
 def triangle(a, b, c)
   raise TriangleError if [a,b,c].min <= 0
 
