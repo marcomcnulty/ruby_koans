845e23df6aa64126299663995b88df52cb5e3a87
marcomcnulty <marcomcnultycodes@gmail.com>
2024-08-21 09:14:24 +0100

    📦 NEW: Completed about dice project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -1,10 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+# TIL:
+# - When expecting a random number(s) not to match e.g. in a unit test, it's best to also check object id in the rare case
+#   that a random number(s) is selected more than once.
+
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values
+
+  def initialize
+    @values = []
+  end
+
+  def roll(number)
+    self.values = []
+    number.times { self.values << rand(1..6) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -31,6 +43,14 @@ class AboutDiceProject < Neo::Koan
     assert_equal first_time, second_time
   end
 
+  # THINK ABOUT IT:
+  #
+  # If the rolls are random, then it is possible (although not
+  # likely) that two consecutive rolls are equal.  What would be a
+  # better way to test this?
+
+  # Answer found here: http://stackoverflow.com/questions/2082970/whats-the-best-way-to-test-this
+
   def test_dice_values_should_change_between_rolls
     dice = DiceSet.new
 
@@ -40,14 +60,8 @@ class AboutDiceProject < Neo::Koan
     dice.roll(5)
     second_time = dice.values
 
-    assert_not_equal first_time, second_time,
-      "Two rolls should not be equal"
-
-    # THINK ABOUT IT:
-    #
-    # If the rolls are random, then it is possible (although not
-    # likely) that two consecutive rolls are equal.  What would be a
-    # better way to test this?
+    assert_not_equal [first_time, first_time.object_id],
+      [second_time, second_time.object_id], "Two rolls should not be equal"
   end
 
   def test_you_can_roll_different_numbers_of_dice
